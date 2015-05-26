d685e453dd793bad23ce739f57a0fb5bdfd856a7
Melos89 <Nicolausfugler_89@hotmail.com>
2015-05-26 13:10:02 +0200

    Added HTML,Razor and DummyModels for homepageview and Homepage Partial views.

---
+++ ProjectHorse/ProjectHorse/Models/HomePageHorsesModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectHorse.Models
+{
+    public class HomePageHorsesModel
+    {
+        public List<string> Horses { get; set; } //Temp list of string, change into Horse objects later 
+
+        public HomePageHorsesModel()
+        {
+            Horses = new List<string>();
+        }
+    }
+}
